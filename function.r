0731e961a07ba26d386a40e60c34dbc0ffc484cf
Rizal-rinaldi <rizalrinaldi37@gmail.com>
2024-12-16 21:59:00 +0700

    function.r

--- function.r
+++ function.r
@@ -1,30 +1,48 @@
-CSQ_genotype <- function (data) {
-  as <- table(data)
-  p <- (as.numeric((2*as[1])+as[2]))/(2*length(data))
-  q <- (as.numeric((2*as[3])+as[2]))/(2*length(data))
-  HO <- as.numeric(as[2]/length(data))
-  HE <- 1-(p^2+q^2)
-  E_Table <- as.vector(c(p^2*length(data), 
-                        2*p*q*length(data), 
-                        q^2*length(data)))
-  O_Table <- as.vector(c((as.numeric(as[1]/length(data)))*(length(data)), 
-                        (as.numeric(as[2]/length(data)))*(length(data)), 
-                        (as.numeric(as[3]/length(data)))*(length(data))))
-  
-  Result <- chisq.test(O_Table, p = E_Table/length(data))
-frquensi_result <- data.frame(
-  Frekuensi_Alel_Dominan = p,
-  Frekuensi_Alel_Resesif = q,
-  HO = HO,
-  HE = HE
-)
-print(frquensi_result)
-result_data <- data.frame(
-  genotype = c(row.names(as)),
-  Observed = O_Table,
-  freq_observed = O_Table/length(data) ,
-  Expected = E_Table,
-  freq_expected = E_Table/length(data))
-print(result_data)
- return(Result)
+CSQ_genotype <- function(data) {
+
+  alleles <- sort(unique(unlist(strsplit(as.character(data), ""))))
+
+
+  genotypes <- unique(apply(expand.grid(alleles, alleles), 1, function(x) paste(sort(x), collapse = "")))
+
+
+  as <- table(factor(data, levels = genotypes))
+
+
+  total_alleles <- 2 * length(data)
+  allele_counts <- table(unlist(strsplit(rep(names(as), as), "")))
+  p <- allele_counts[alleles[1]] / total_alleles
+  q <- allele_counts[alleles[2]] / total_alleles
+
+
+  HO <- sum(as[grep(paste0("[", alleles[1], "]", "[", alleles[2], "]"), names(as))]) / length(data)
+  HE <- 1 - (p^2 + q^2)
+
+
+  E_Table <- c(p^2 * length(data), 2 * p * q * length(data), q^2 * length(data))
+
+  O_Table <- as.vector(as)
+
+
+  Result <- chisq.test(O_Table, p = E_Table / length(data))
+
+
+  frekuensi_result <- data.frame(
+    Frekuensi_Alel_Dominan = p,
+    Frekuensi_Alel_Resesif = q,
+    HO = HO,
+    HE = HE
+  )
+  print(frekuensi_result)
+
+  result_data <- data.frame(
+    Genotype = names(as),
+    Observed = O_Table,
+    Freq_Observed = O_Table / length(data),
+    Expected = E_Table,
+    Freq_Expected = E_Table / length(data)
+  )
+  print(result_data)
+
+  return(Result)
 }
